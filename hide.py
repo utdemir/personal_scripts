83f29eecf8739a2993860d84df45878642e82803
Utku Demir <utdemir@gmail.com>
2013-12-06 13:34:14 +0200

    Add hide.py

---
+++ hide.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import hashlib
+import itertools
+import functools
+
+def invalid_arguments():
+    print("Invalid arguments.", file=sys.stderr) # TODO: Usage
+    sys.exit(1)
+
+_, secrets_file, mode, *files = sys.argv
+
+if mode not in ("hide", "reveal"):
+    invalid_arguments()
+
+secrets_file = os.path.abspath(secrets_file)
+
+files = list(map(os.path.abspath, files))
+if not all(map(os.path.isfile, files)):
+    invalid_arguments()
+
+files = set(files) - {secrets_file} # To allow `./hide.py secrets lock *`
+
+salt, *secrets = [i.strip() for i in open(secrets_file) if i.strip()]
+
+def hash(secret, fname):
+    key = "%s:%s:%s" % (salt, fname, secret)
+    return hashlib.sha256(key.encode()).hexdigest()
+
+for filepath in files:
+    filename = os.path.basename(filepath) 
+    hashes = [(i, hash(i, filename)) for i in secrets] 
+    with open(filename, "r") as f:
+       contents = f.read()
+       for s, h in hashes:
+           if mode == "hide":
+               contents = contents.replace(s, h)
+           elif mode == "reveal":
+               contents = contents.replace(h, s)
+    with open(filename, "w") as f:
+       f.write(contents)
