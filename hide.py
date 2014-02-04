6e4b10282c1b1b29eee08b33765b75b59656c935
Utku Demir <utdemir@gmail.com>
2014-02-04 15:29:24 +0200

    hide.py: Handle directories recursively

--- hide.py
+++ hide.py
@@ -11,20 +11,34 @@ def invalid_arguments():
     sys.exit(1)
 
 try:
-    _, secrets_file, mode, *files = sys.argv
+    _, secrets_file, mode, *args = sys.argv
 except ValueError:
     invalid_arguments()
 
 if mode not in ("hide", "reveal", "ensure"):
     invalid_arguments()
 
-secrets_file = os.path.abspath(secrets_file)
+args = set(map(os.path.abspath, args))
 
-files = list(map(os.path.abspath, files))
-if not all(map(os.path.isfile, files)):
+secrets_file = os.path.abspath(secrets_file)
+args = args - {secrets_file} # To allow `./hide.py secrets lock *`
+
+files = set(filter(os.path.isfile, args))
+dirs = set(filter(os.path.isdir, args))
+
+if len(files) + len(dirs) != len(args):
     invalid_arguments()
 
-files = set(files) - {secrets_file} # To allow `./hide.py secrets lock *`
+def list_files(dir):
+    contents = [os.path.join(dir, i) for i in os.listdir(dir)]
+    files = set(filter(os.path.isfile, contents))
+    dirs = {i for i in contents if os.path.isdir(i) and not i.startswith(".")}
+
+    yield from files
+    yield from itertools.chain(*map(list_files, dirs))
+
+all_files = files | set(itertools.chain(*map(list_files, dirs)))
+from pprint import pprint
 
 salt, *secrets = [i.strip() for i in open(secrets_file) if i.strip()]
 secrets.sort(key=len, reverse=True)
@@ -33,7 +47,7 @@ def hash(secret, fname):
     key = "%s:%s:%s" % (salt, fname, secret)
     return "hidepy" + hashlib.sha256(key.encode()).hexdigest()
 
-for filepath in files:
+for filepath in all_files:
     filename = os.path.basename(filepath) 
     hashes = [(i, hash(i, filename)) for i in secrets] 
     with open(filepath, "r") as f:
